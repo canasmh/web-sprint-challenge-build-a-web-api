02aa48a02ef346d62416c4ab77e4e3fc7c1b2823
canasmh <canasmh@yahoo.com>
2023-01-10 15:21:48 -0500

    Use the project Router

--- api/server.js
+++ api/server.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const projectsRouter = require('./projects/projects-router');
 const server = express();
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+
+// server.get('/', (req, res) => {
+//     res.send('API up and running!');
+// })
+
+server.use('/api/projects', projectsRouter);
+
+server.get('/', (req, res) => {
+    console.log('on the home route')
+    res.send('Hello from the GET /')
+})
 
 module.exports = server;
