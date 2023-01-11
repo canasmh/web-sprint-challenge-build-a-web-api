4cbfb9566dae21c941c8fa419862f1de6dc96da3
canasmh <canasmh@yahoo.com>
2023-01-11 14:30:26 -0500

    Use newly created middleware functions in the actions router

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -4,6 +4,7 @@ const express = require('express');
 const router = express.Router();
 const actionMethods = require('./actions-model');
 const projectMethods = require('../projects/projects-model');
+const { checkParams, checkCompleted, checkActionID, checkProjectID } = require('./actions-middleware');
 
 router.get('/', async (req, res) => {
     // RETURNS AN ARRAY OF ACTIONS (OR AN EMPTY ARRAY) AS THE BODY OF THE RESPONSE;
@@ -12,99 +13,53 @@ router.get('/', async (req, res) => {
     res.status(200).send(data);
 })
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', checkActionID, async (req, res, next) => {
     // Returns an action with the given id as the body of the response.
     // if there is no action with the given id it responds with a status code 404
-    const id = req.params.id;
-    const action = await actionMethods.get(id);
-
-    if (!action) {
-        res.sendStatus(404);
-    } else {
+    try {
+        const action = await actionMethods.get(req.params.id);
         res.status(200).json(action);
+    } catch (err) {
+        next(err);
     }
 })
 
-router.post('/', async (req, res) => {
+router.post('/', checkParams, async (req, res, next) => {
     // Returns the newly created action as the body of the response.
     // If the request body is missing any of the required fields it responds with a status code 400.
     // when adding an action make sure the project_id provided belongs to an existing project
-    const project_id = req.query.project_id;
-    const description = req.query.description;
-    const notes = req.query.notes;
-    const completed = req.query.completed;
-
-    if (project_id === undefined || description === undefined || notes === undefined) {
-        res.sendStatus(400);
-
-    } else {
-        const projectData = await projectMethods.get(project_id)
-
-        if (!projectData) {
-            res.sendStatus(400);
-
-        } else {
-            const newData = {
-                "project_id": project_id,
-                "description": description,
-                "notes": notes,
-                "completed": completed === undefined ? false : completed
-            };
-    
-            const data = await actionMethods.insert(newData);
-            res.status(200).json(newData);
-        }   
+    try {
+        const insertedAction = await actionMethods.insert(req.body);
+        res.status(201).send(insertedAction)
+    } catch (err) {
+        next(err);
     }
+
 })
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', checkActionID, checkParams, checkCompleted, async (req, res, next) => {
 
     // Returns the updated action as the body of the response.
     // If there is no action with the given id it responds with a status code 404.
     // If the request body is missing any of the required fields it responds with a status code 400.
-
-    const id = req.params.id;
-    const currentData = await actionMethods.get(id);
-    const project_id = req.query.project_id;
-    const description = req.query.description;
-    const notes = req.query.notes;
-    const completed = req.query.completed;
-
-    if (!currentData) {
-        res.sendStatus(404);
-
-    } else {
-        if (project_id === undefined || description === undefined || notes === undefined || completed === undefined) {
-            res.sendStatus(400);
-        } else {
-
-            const newData = {
-                [project_id]: project_id,
-                [description]: description,
-                [notes]: notes,
-                [completed]: completed
-            }
-
-            const data = await actionMethods.update(id, newData);
-
-            res.status(200).send(data);
-        }
+    try {
+        const updatedAction = await actionMethods.update(req.params.id, req.body);
+        res.status(200).send(updatedAction);
+    } catch(err) {
+        next(err);
     }
-    
 })
 
-router.delete('/:id', async (req, res) => {
-    const id = req.params.id;
-    const action = await actionMethods.get(id);
-
-    if (!action) {
-        res.sendStatus(404);
-    } else {
-        const del = await actionMethods.remove(id);
-        res.sendStatus(200);
-    }
+router.delete('/:id', checkActionID, async (req, res, next) => {
     // Returns no response body.
     // If there is no action with the given id it responds with a status code 404.
+
+    try {
+        const deletedAction = await actionMethods.remove(req.params.id);
+        res.sendStatus(204);
+    } catch(err) {
+        next(err);
+    }
 })
 
 module.exports = router;
