e40ba95c2b91aa473848d37cabfc4a390b8a8451
canasmh <canasmh@yahoo.com>
2023-01-11 13:32:59 -0500

    Use json and url encoded middleware

--- api/server.js
+++ api/server.js
@@ -8,12 +8,20 @@ const server = express();
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
 
+server.use(express.json());
+server.use(express.urlencoded());
 server.use('/api/projects', projectsRouter);
 server.use('/api/actions', actionsRouter);
+
+
 
 server.get('/', (req, res) => {
     console.log('on the home route')
     res.send('Hello from the GET /')
+})
+
+server.use((err, req, res, next) => {
+    res.status(err.status || 500).json({message: process.env.NODE_ENV === 'PRODUCTION' ? 'sorry, there was an erorr' : err.message})
 })
 
 module.exports = server;
